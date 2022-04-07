996e42ea3cf0b6095cc20688d35da2988a3ff9a9
Alberto <alby0705@gmail.com>
2022-04-07 22:22:49 +0200

    c# and java basic templates

---
+++ templates/c#/basic/main.cs
@@ -0,0 +1,8 @@
+// using System
+
+class ClassName {         
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello World!");
+    }
+}
---
+++ templates/java/basic/main.java
@@ -0,0 +1,7 @@
+package templates.java.basic;
+
+class ClassName{  
+    public static void main(String args[]){  
+        System.out.println("Hello World!");  
+    }  
+}  
